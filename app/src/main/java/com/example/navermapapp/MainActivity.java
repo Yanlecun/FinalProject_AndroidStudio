0e5a6b753bd30d3a21b22c28557e8f3c79274a07
songwoo <thddn191@gmail.com>
2021-05-31 21:34:21 +0900

    Initial commit(layout.xml/ string, style, drawable/ MainActivity slightly amended)

---
+++ app/src/main/java/com/example/navermapapp/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.navermapapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView st, li;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        st.findViewById(R.id.start);
+        li.findViewById(R.id.list);
+
+        st.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        li.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+}
