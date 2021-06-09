adea85257d3161bc2b1c0dcaabdecbada7ae7078
songwoo <thddn191@gmail.com>
2021-06-09 23:34:24 +0900

    commit9 (restaurant+List+PDB+Map 완성2 + FirstActivity )

---
+++ app/src/main/java/com/example/navermapapp/FirstActivity.java
@@ -0,0 +1,20 @@
+package com.example.navermapapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class FirstActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first);
+    }
+    public void onClickNext(View v){
+        Intent it = new Intent(getApplicationContext(), MainActivity.class);
+        startActivity(it);
+    }
+}
